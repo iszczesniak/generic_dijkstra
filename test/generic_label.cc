9cdec033340ea89c1d8a2b618999d4b414cf99ae
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-03-19 23:11:29 +0100

    label

--- test/generic_label.cc
+++ test/generic_label.cc
@@ -5,48 +5,6 @@ using label = generic_label<double, cunits<int>>;
 
 using namespace std;
 
-void
-test_generic_label()
-{
-}
-
-list<label>
-worse(const label &li)
-{
-  list<label> l;
-
-  return l;
-}
-
-// *****************************************************************
-// transitivity
-// *****************************************************************
-void
-test_transitivity()
-{
-  // This label could be any.
-  label li(10, {10, 20});
-
-  for(const auto &lj: worse(li))
-    for(const auto &lk: worse(lj))
-      {
-        assert(li < lj && lj < lk);
-        assert(li < lk);
-
-        // Test the default implementation of >.
-        assert(lk > lj && lj > li);
-        assert(lk > li);
-
-        // Test the default implementation of <=.
-        assert(li <= lj && lj <= lk);
-        assert(li <= lk);
-
-        // Test the default implementation of >=.
-        assert(lk >= lj && lj >= li);
-        assert(lk >= li);
-      }
-}
-
 // *****************************************************************
 // Test all relations
 // *****************************************************************
@@ -74,12 +32,108 @@ test_relations()
                get_resource(li).max() - 1});
     assert(li < lj3);
   }
+
+  // Row 1, column 2.
+  {
+    label lj(get_cost(li) + 1, get_resource(li));
+    assert(li < lj);
+  }
+
+  // Row 1, column 3.
+  {
+    label lj1(get_cost(li) + 1,
+              {get_resource(li).min() - 1,
+               get_resource(li).max()});
+    assert(li < lj1);
+
+    label lj2(get_cost(li) + 1,
+              {get_resource(li).min(),
+               get_resource(li).max() + 1});
+    assert(li < lj2);
+
+    label lj3(get_cost(li) + 1,
+              {get_resource(li).min() - 1,
+               get_resource(li).max() + 1});
+    assert(li < lj3);
+  }
+
+  // Row 1, column 4.
+  {
+    label lj1(get_cost(li) + 1,
+              {get_resource(li).min() - 1,
+               get_resource(li).max() - 1});
+    assert(li < lj1);
+
+    label lj2(get_cost(li) + 1,
+              {get_resource(li).min() + 1,
+               get_resource(li).max() + 1});
+    assert(li < lj2);
+
+    label lj3(get_cost(li) + 1,
+              {get_resource(li).min() - 1,
+               get_resource(li).min()});
+    assert(li < lj3);
+
+    label lj4(get_cost(li) + 1,
+              {get_resource(li).max(),
+               get_resource(li).max() + 1});
+    assert(li < lj4);
+
+    label lj5(get_cost(li) + 1,
+              {get_resource(li).min() - 2,
+               get_resource(li).min() - 1});
+    assert(li < lj5);
+
+    label lj6(get_cost(li) + 1,
+              {get_resource(li).max() + 1,
+               get_resource(li).max() + 2});
+    assert(li < lj6);
 }
+
+}
+
+// *****************************************************************
+// transitivity
+// *****************************************************************
+
+list<label>
+worse(const label &li)
+{
+  list<label> l;
+
+  return l;
+}
+
+void
+test_transitivity()
+{
+  // This label could be any.
+  label li(10, {10, 20});
+
+  for(const auto &lj: worse(li))
+    for(const auto &lk: worse(lj))
+      {
+        assert(li < lj && lj < lk);
+        assert(li < lk);
+
+        // Test the default implementation of >.
+        assert(lk > lj && lj > li);
+        assert(lk > li);
+
+        // Test the default implementation of <=.
+        assert(li <= lj && lj <= lk);
+        assert(li <= lk);
+
+        // Test the default implementation of >=.
+        assert(lk >= lj && lj >= li);
+        assert(lk >= li);
+      }
+}
+
 
 int
 main()
 {
-  test_generic_label();
-  test_transitivity();
   test_relations();
+  test_transitivity();
 }
