bc5804c4dbe423dcd26459f50fe0f39e3ea8bd8d
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2022-04-04 20:57:21 +0200

    label

--- test/generic_label.cc
+++ test/generic_label.cc
@@ -25,7 +25,7 @@ better(const label &li, const label &lj)
 
 // Make sure that li == lj.
 void
-eq(const label &li, const label &lj)
+equal(const label &li, const label &lj)
 {
   assert(li == lj);
   assert(!(li != lj));
@@ -39,17 +39,12 @@ eq(const label &li, const label &lj)
 void
 worse(const label &li, const label &lj)
 {
-  assert(li > lj);
-  assert(li >= lj);
-  assert(li != lj);
-  assert(!(li == lj));
-  assert(!(li < lj));
-  assert(!(li <= lj));
+  better(lj, li);
 }
 
 // Make sure that li || lj.
 void
-ic(const label &li, const label &lj)
+incomparable(const label &li, const label &lj)
 {
   assert(!(li < lj));
   assert(!(li > lj));
@@ -137,7 +132,7 @@ icr_RIs(const CU &ri)
 
 // Returns RIs rj such that: ri || rj
 auto
-ic_RIs(const CU &ri)
+incomparable_RIs(const CU &ri)
 {
   list<CU> l1 = icl_RIs(ri);
   list<CU> l2 = icr_RIs(ri);
@@ -158,216 +153,71 @@ test_relations()
   label li(10, {100, 120});
 
   // -----------------------------------------------------------------
-  // Row 1, column 1.
+  // Column 1.
+  for(auto &rj: worse_RIs(get_resources(li)))
+    {
+      // Row 1.
+      label lj1(get_cost(li) + 1, rj);
+      better(li, lj1);
+
+      // Row 2.
+      label lj2(get_cost(li), rj);
+      better(li, lj2);
+
+      // Row 3.
+      label lj3(get_cost(li) - 1, rj);
+      worse(li, lj3);
+    }
+
+  // Column 2.
   {
-    label lj1(get_cost(li) + 1,
-              {get_resources(li).min() + 1,
-               get_resources(li).max()});
+    // Row 1.
+    label lj1(get_cost(li) + 1, get_resources(li));
     better(li, lj1);
 
-    label lj2(get_cost(li) + 1,
-              {get_resources(li).min(),
-               get_resources(li).max() - 1});
-    better(li, lj2);
+    // Row 2.
+    label lj2(get_cost(li), get_resources(li));
+    equal(li, lj2);
 
-    label lj3(get_cost(li) + 1,
-              {get_resources(li).min() + 1,
-               get_resources(li).max() - 1});
-    better(li, lj3);
-  }
-
-  // Row 1, column 2.
-  {
-    label lj(get_cost(li) + 1, get_resources(li));
-    better(li, lj);
-  }
-
-  // Row 1, column 3.
-  {
-    label lj1(get_cost(li) + 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).max()});
-    better(li, lj1);
-
-    label lj2(get_cost(li) + 1,
-              {get_resources(li).min(),
-               get_resources(li).max() + 1});
-    better(li, lj2);
-
-    label lj3(get_cost(li) + 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).max() + 1});
-    better(li, lj3);
-  }
-
-  // Row 1, column 4.
-  {
-    label lj1(get_cost(li) + 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).max() - 1});
-    better(li, lj1);
-
-    label lj2(get_cost(li) + 1,
-              {get_resources(li).min() + 1,
-               get_resources(li).max() + 1});
-    better(li, lj2);
-
-    label lj3(get_cost(li) + 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).min()});
-    better(li, lj3);
-
-    label lj4(get_cost(li) + 1,
-              {get_resources(li).max(),
-               get_resources(li).max() + 1});
-    better(li, lj4);
-
-    label lj5(get_cost(li) + 1,
-              {get_resources(li).min() - 2,
-               get_resources(li).min() - 1});
-    better(li, lj5);
-
-    label lj6(get_cost(li) + 1,
-              {get_resources(li).max() + 1,
-               get_resources(li).max() + 2});
-    better(li, lj6);
-  }
-
-  // -----------------------------------------------------------------
-  // Row 2, column 1.
-  {
-    label lj1(get_cost(li),
-              {get_resources(li).min() + 1,
-               get_resources(li).max()});
-    better(li, lj1);
-
-    label lj2(get_cost(li),
-              {get_resources(li).min(),
-               get_resources(li).max() - 1});
-    better(li, lj2);
-
-    label lj3(get_cost(li),
-              {get_resources(li).min() + 1,
-               get_resources(li).max() - 1});
-    better(li, lj3);
-  }
-
-  // Row 2, column 2.
-  {
-    label lj(get_cost(li), get_resources(li));
-    eq(li, lj);
-  }
-
-  // Row 2, column 3.
-  {
-    label lj1(get_cost(li),
-              {get_resources(li).min() - 1,
-               get_resources(li).max()});
-    worse(li, lj1);
-
-    label lj2(get_cost(li),
-              {get_resources(li).min(),
-               get_resources(li).max() + 1});
-    worse(li, lj2);
-
-    label lj3(get_cost(li),
-              {get_resources(li).min() - 1,
-               get_resources(li).max() + 1});
+    // Row 3.
+    label lj3(get_cost(li) - 1, get_resources(li));
     worse(li, lj3);
   }
 
-  // Row 2, column 4.
-  {
-    // li < lj
-    for(const auto &cu: worse_RIs(get_resources(li)))
-      {
-        label lj(get_cost(li), cu);
-        better(li, lj);
-      }
+  // Column 3.
+  for(auto &rj: better_RIs(get_resources(li)))
+    {
+      // Row 1.
+      label lj1(get_cost(li) + 1, rj);
+      better(li, lj1);
 
-    // li > lj
-    for(const auto &cu: better_RIs(get_resources(li)))
-      {
-        label lj(get_cost(li), cu);
-        worse(li, lj);
-      }
-  }
+      // Row 2.
+      label lj2(get_cost(li), rj);
+      worse(li, lj2);
 
-  // -----------------------------------------------------------------
-  // Row 3, column 1.
-  {
-    label lj1(get_cost(li) - 1,
-              {get_resources(li).min() + 1,
-               get_resources(li).max()});
-    worse(li, lj1);
+      // Row 3.
+      label lj3(get_cost(li) - 1, rj);
+      worse(li, lj3);
+    }
 
-    label lj2(get_cost(li) - 1,
-              {get_resources(li).min(),
-               get_resources(li).max() - 1});
-    worse(li, lj2);
+  // Column 4.
+  for(auto &rj: incomparable_RIs(get_resources(li)))
+    {
+      // Row 1.
+      label lj1(get_cost(li) + 1, rj);
+      better(li, lj1);
 
-    label lj3(get_cost(li) - 1,
-              {get_resources(li).min() + 1,
-               get_resources(li).max() - 1});
-    worse(li, lj3);
-  }
+      // Row 2.
+      label lj2(get_cost(li), rj);
+      if (get_resources(li) < rj)
+        better(li, lj2);
+      else
+        worse(li, lj2);
 
-  // Row 3, column 2.
-  {
-    label lj(get_cost(li) - 1, get_resources(li));
-    worse(li, lj);
-  }
-
-  // Row 3, column 3.
-  {
-    label lj1(get_cost(li) - 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).max()});
-    worse(li, lj1);
-
-    label lj2(get_cost(li) - 1,
-              {get_resources(li).min(),
-               get_resources(li).max() + 1});
-    worse(li, lj2);
-
-    label lj3(get_cost(li) - 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).max() + 1});
-    worse(li, lj3);
-  }
-
-  // Row 3, column 4.
-  {
-    label lj1(get_cost(li) - 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).max() - 1});
-    worse(li, lj1);
-
-    label lj2(get_cost(li) - 1,
-              {get_resources(li).min() + 1,
-               get_resources(li).max() + 1});
-    worse(li, lj2);
-
-    label lj3(get_cost(li) - 1,
-              {get_resources(li).min() - 1,
-               get_resources(li).min()});
-    worse(li, lj3);
-
-    label lj4(get_cost(li) - 1,
-              {get_resources(li).max(),
-               get_resources(li).max() + 1});
-    worse(li, lj4);
-
-    label lj5(get_cost(li) - 1,
-              {get_resources(li).min() - 2,
-               get_resources(li).min() - 1});
-    worse(li, lj5);
-
-    label lj6(get_cost(li) - 1,
-              {get_resources(li).max() + 1,
-               get_resources(li).max() + 2});
-    worse(li, lj6);
-  }
+      // Row 3.
+      label lj3(get_cost(li) - 1, rj);
+      worse(li, lj3);
+    }
 }
 
 // *****************************************************************
@@ -508,9 +358,8 @@ test_boe_incomparability()
       // The cost is be higher, the CU better.
       label lj(get_cost(li) + 1, rj);
 
-      
       // Second relation: row 1, column 4
-      for (const auto &rk: ic_RIs(get_resources(lj)))
+      for (const auto &rk: incomparable_RIs(get_resources(lj)))
         {
           // The cost is higher, the CU incomparable.
           label lj(get_cost(lj) + 1, rk);
