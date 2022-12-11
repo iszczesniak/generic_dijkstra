e02cbad6082706c0bdce0d3f23b728348352408d
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-12-11 21:43:17 +0100

    generic

--- generic_label.hpp
+++ generic_label.hpp
@@ -43,4 +43,13 @@ operator<<(std::ostream &out,
     return out;
 }
 
+// *******************************************************************
+// The weight traits
+
+template <typename Weight, typename Resources>
+struct weight_traits<generic_label<Weight, Resources>>
+{
+  using type = Weight;
+};
+
 #endif // GENERIC_LABEL_HPP
