4b36d5d8a0a5674d40a630d484d6eb0b20d79fa4
Irek Szcześniak <irek.szczesniak@gmail.com>
2024-04-09 14:40:20 +0200

    test

--- test/generic_permanent2.cc
+++ test/generic_permanent2.cc
@@ -2,16 +2,31 @@
 #include "label_robe.hpp"
 #include "units.hpp"
 
-using robed_label = label_robe<CU>;
-using label = robed_label::label_type;
-
 void
-boe()
+boe_cu()
 {
+  using robed_label = label_robe<CU>;
+  using label = robed_label::label_type;
+
   generic_permanent2<robed_label> P(1);
 
   label l1(1, {0, 2});
   label l2(2, {0, 1});
+
+  P.push(robed_label(l1, 0));
+  assert(has_better_or_equal(P, robed_label(l2, 0)));
+}
+
+void
+boe_su()
+{
+  using robed_label = label_robe<SU>;
+  using label = robed_label::label_type;
+
+  generic_permanent2<robed_label> P(1);
+
+  label l1(1, {{0, 1}, {2, 3}});
+  label l2(2, {{0, 1}});
 
   P.push(robed_label(l1, 0));
   assert(has_better_or_equal(P, robed_label(l2, 0)));
